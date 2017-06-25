4e0f2a6e64829d47e7908b38d93815d710310477
Productive Programmer <charles.lobo@gmail.com>
2017-06-25 23:07:45 +1000

    Generating combinations blog

---
+++ GeneratingCombinations.java
@@ -0,0 +1,148 @@
+/**
+ * How to Generate Combinations
+ */
+/**
+ * Occasionally we come across
+ * cases where we have to
+ * generate all possible
+ * combinations of a set.
+ * We know that the natural
+ * solution is recursive but
+ * I've found I struggle with
+ * getting it right.
+ *
+ * If you are in the same
+ * situation, I'd like to share
+ * my favorite method for
+ * generating combinations in
+ * this post.
+ */
+/**
+ * combinations.png
+ */
+/**
+ * There are two possible
+ * solutions that I know of for
+ * generating combinations –
+ * recursive and iterative. My
+ * favorite is the iterative
+ * because it uses a really neat
+ * trick, but I’ll start with
+ * explaining the elegant
+ * recursive solution.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+class GeneratingCombinations {
+
+/**
+ * *_The Recursive Method_*
+ */
+/**
+ * In this method, we go through
+ * each element and either
+ *  (a) Add it to the
+ *  combination or
+ *  (b) Not add it to the
+ *  combination
+ * Then, whenever we reach the
+ * last element we have
+ * generated a new combination!
+ */
+/**
+ * recursive-tree.png
+ */
+/**
+ * Simple? Let's try it below.
+ **/
+private void recursive_combinations(List<Object> combination,
+                                      int ndx, Object[] elems) {
+    if(ndx == elems.length) {
+
+        // (reached end of list after selecting/not selecting)
+        System.out.println(combination);
+
+    } else {
+
+        // (include element at ndx)
+        combination.add(elems[ndx]);
+        recursive_combinations(combination, ndx+1, elems);
+
+        // (don't include element at ndx)
+        combination.remove(elems[ndx]);
+        recursive_combinations(combination, ndx+1, elems);
+
+    }
+}
+public void recursive_combinations_start(Object[] elems) {
+    List<Object> combination = new ArrayList<>();
+    recursive_combinations(combination, 0, elems);
+}
+
+/**
+ * *__The Iterative Method__*
+ */
+/**
+ * Now for my favorite method. I
+ * love this because it uses a
+ * little trick – think about
+ * the elements “standing” one
+ * next to the other. Under it
+ * we imagine a little switch –
+ * “on” if the element is in the
+ * set, “off” if it isn’t:
+ */
+/**
+ * elements-in-line.png
+ */
+/**
+ * On…off…on…off… sounds
+ * familiar? It “looks like” the
+ * binary representation of a
+ * number. And what I find so
+ * delightful is that – yes – we
+ * _can_ use the binary
+ * representation of a number to
+ * generate all possible
+ * combinations! The numbers
+ * from 0 to (2^n-1) contain, in
+ * their bits, all the
+ * combinations we need! This is
+ * simple and fun to code:
+ */
+public void iterate_combinations(Object[] elems) {
+    int n = elems.length;
+    for(int num = 0;num < (1 << n);num++) {
+        List<Object> combination = new ArrayList<>();
+        for(int ndx = 0;ndx < n;ndx++) {
+            // (is the bit "on" in this number?)
+            if((num & (1 << ndx)) != 0) {
+                // then it's included in the list
+                combination.add(elems[ndx]);
+            }
+        }
+        // (show the current combination)
+        System.out.println(combination);
+    }
+}
+
+/**
+ * Which method is your
+ * favorite? Do you know any
+ * other methods? Let me know in
+ * the comments below.
+ **/
+
+public static void main(String[] args) {
+    String elems[] = { "1", "2", "3", "4", "5" };
+
+    System.out.println("Recursive combinations");
+    new GeneratingCombinations().recursive_combinations_start(elems);
+    System.out.println("Iterate combinations");
+    new GeneratingCombinations().iterate_combinations(elems);
+}
+
+}
+
